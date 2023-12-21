4370e5b063d30cca8d7772968501b91e8776a4aa
kp223 <122981677+kp223@users.noreply.github.com>
2023-12-20 21:42:37 -0500

    Update tensor_generator.R

--- code/functions/tensor_generator.R
+++ code/functions/tensor_generator.R
@@ -7,6 +7,7 @@ dir_data=(args[2])
 dir_functions=(args[3])
 chr_num=as.numeric(args[4])
 sizefile=(args[5])
+binsize=as.numeric(args[6])
 
 #ssh=args[4]
 ######
@@ -32,7 +33,7 @@ registerDoParallel(cl)
 chrlist=paste0("chr",c(1:(chr_num-1),"X"))
 
 
-summarized_hic=qs::qread(paste0(dir_data,'/summarized_hic.qs'))
+summarized_hic=qs::qread(paste0(dir_data,"/summarized_hic_",chrlist[chr],".qs"))#qs::qread(paste0(dir_data,'/summarized_hic.qs'))
 
 #Comeback this part
 
@@ -41,7 +42,7 @@ size<-fread(paste0(dir_data,'/',sizefile))
 size<-size[size$V1%in%chrlist,]
 size<-size[size$V1 %>% mixedorder,]
 
-binsize=1000000
+
 dimlist=ceiling(size$V2[size$V1%in%chrlist]/binsize)
 
 #dimlist=readRDS('dimlist_mm9.rds')
@@ -49,9 +50,9 @@ dimlist=ceiling(size$V2[size$V1%in%chrlist]/binsize)
 #cell by locpair
 
 
-fitted=qs::qread(paste0(dir_data,"/Matricized_HiCtensor_imputed_chr",chr,".qs"))
+fitted=qs::qread(paste0(dir_data,"/Matricized_HiCtensor_imputed_",chrlist[chr],".qs"))
 
-summarized_hic=summarized_hic[which(summarized_hic$chrom==chrlist[chr]),]
+#summarized_hic=summarized_hic[which(summarized_hic$chrom==chrlist[chr]),]
 
 
 
@@ -63,31 +64,31 @@ num_cell=dim(fitted)[1]
 library(foreach)
 #,.export = c("sparse2dense","balance")
 tmp_list=foreach(c=1:num_cell,.packages = c("Matrix",  "dplyr"))%dopar%{
-                   
-                   print(c)
-                   tmp=cbind(summarized_hic,fitted[c,])  
-                   #      tmp=hic_df %>% filter(chrom=="chr1",cell=="GM12878_1")
-                   tmp<-tmp%>%select(-c(chrom))
-                   #tmp<-tmp%>%select(-c(chrom,diag,cell))
-                   dimnames(tmp)[[2]]=c("i","j","val")
-                   tmp$i=tmp$i/1000000+1
-                   tmp$j=tmp$j/1000000+1
-                   
-                   aa=sparse2dense(tmp,N=dimlist[chr])
-                   
-                   return(aa) 
-                   
-                   
-                   
-                   
-                 }
+  
+  print(c)
+  tmp=cbind(summarized_hic,fitted[c,])  
+  #      tmp=hic_df %>% filter(chrom=="chr1",cell=="GM12878_1")
+  tmp<-tmp%>%select(-c(chrom))
+  #tmp<-tmp%>%select(-c(chrom,diag,cell))
+  dimnames(tmp)[[2]]=c("i","j","val")
+  tmp$i=tmp$i/binsize+1
+  tmp$j=tmp$j/binsize+1
+  
+  aa=sparse2dense(tmp,N=dimlist[chr])
+  
+  return(aa) 
+  
+  
+  
+  
+}
 
 
 
 tmp_tensor=do.call("abind",c(tmp_list, list(along=3)))
 
 
-    
+
 
 
 print(chrlist[chr])
