84269c944e8094994828ea727a853938825df5c8
kp223 <122981677+kp223@users.noreply.github.com>
2023-12-20 22:12:09 -0500

    Update config_file_preprocess.R

--- code/wrapper/config_file_preprocess.R
+++ code/wrapper/config_file_preprocess.R
@@ -1,10 +1,9 @@
-
 #number of chromosomes (23(Human) or 20(Mouse))
 chr_num=20
 
 
 #directory of the data
-dir_data="{Muscle directory}/data/Liu2021"
+dir_data="{Muscle directory}/data/Li2019"
 
 #directory of the functions
 dir_functions="{Muscle directory}/code/functions"
@@ -13,8 +12,17 @@ dir_functions="{Muscle directory}/code/functions"
 #Initial maximum rank of SVD of HiC
 exploration_rank=150
 
+#Ranks for each data 
+#( 0 (default) : it means to choose rank based on the screeplot of SVD, e.g., Rank_HiC=0 means that the rank for Hi-C data is chosen based on SVD of matricized Hi-C.)
+#( R : it means to set the rank as R, e.g., Rank_HiC=30 means that the rank for Hi-C data is 30. R is nonzero in this case.)
+
+Rank_HiC=0
+Rank_mCG=0
+Rank_mCH=0
+
+
 #Do you want debiasing? (Yes: TRUE, No: FALSE)
-debias=TRUE
+debias=FALSE
 
 #Replace only zero entries with impuation (Yes: TRUE, No: FALSE)
 only_zero_entries=FALSE
@@ -33,6 +41,9 @@ ssh=NULL
 
 
 #chromosome size file name within the data directory.
-#sizefile='mm9.chrom.sizes'
-sizefile='mm10.chrom.sizes'
+sizefile='mm9.chrom.sizes'
+#sizefile='mm10.chrom.sizes'
 #sizefile='hg19.chrom.sizes'
+
+#binsize determines the resolution of the data (default=1000000)
+binsize=1000000
