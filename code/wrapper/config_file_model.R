2f02fc2bdd3d5ec38cc334d6e83c7fc2b6e3e664
kp223 <122981677+kp223@users.noreply.github.com>
2023-12-20 22:21:10 -0500

    Update config_file_model.R

--- code/wrapper/config_file_model.R
+++ code/wrapper/config_file_model.R
@@ -1,15 +1,11 @@
-
-
-
 #Directory of data: The directory should contain hic_df.qs and chrom.sizes file.
-dir_data="{Muscle directory}/data/Liu2021"
+dir_data="{Muscle directory}/data/Li2019"
 
 #Directory of functions
 dir_functions="{Muscle directory}/code/functions"
 
 #Directory where Muscle output goes into
-
-dir_out="{Muscle directory}/results/Liu2021"
+dir_out="{Muscle directory}/results/Li2019"
 
 
 
@@ -19,9 +15,19 @@ exploration_rank=150
 
 #Modality of the tensors. #(only 3 choices available : "All", or "HiC", or "HiC+CG")
 
-modality="All" #When all Hi-C, mCG, mCH are analyzed
+#modality="All" #When all Hi-C, mCG, mCH are analyzed
 #modality="HiC" #When only Hi-C is analyzed
-#modality="HiC+CG" #when only Hi-C and mCG are analyzed
+modality="HiC+CG" #when only Hi-C and mCG are analyzed
+
+
+#Ranks for each data 
+#( 0 (default) : it means to choose rank based on the screeplot of SVD, e.g., Rank_HiC=0 means that the rank for Hi-C data is chosen based on SVD of Hi-C.)
+#( R : it means to set the rank as R, e.g., Rank_HiC=30 means that the rank for Hi-C data is 30. R is nonzero in this case.)
+
+Rank_HiC=15
+Rank_mCG=10
+Rank_mCH=0
+
 
 
 # If bulk TAD information exists, set it as TRUE. Otherwise, FALSE
@@ -37,7 +43,7 @@ tol=0.00001
 
 
 #maximum number of iterations for each rank 1 update
-maxiter=10
+maxiter=5
 
 #server names for gnu parallel. 
 #If you are not using multiple servers for GNU parallel, leave it as ssh=NULL
